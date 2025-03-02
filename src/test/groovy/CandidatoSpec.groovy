59eba06bb12091bc193b67a45ffcf6ddaca22d21
Jenyfferbastos <jenyfferbastos06@gmail.com>
2025-03-02 15:36:05 -0300

    test: adiciona testes para Candidato e Empresa

---
+++ src/test/groovy/CandidatoSpec.groovy
@@ -0,0 +1,30 @@
+import spock.lang.Specification
+import src.models.Candidato
+
+class CandidatoSpec extends Specification {
+    def "Deve adicionar um candidato na lista"() {
+        given: "Uma lista vazia de candidatos"
+        List<Candidato> candidatos = []
+
+        and: "Um novo candidato"
+        Candidato candidato = new Candidato(
+                "Alice",
+                "alice@email.com",
+                "123.456.789-00",
+                30,
+                "São Paulo",
+                "01000-000",
+                "Desenvolvedora Java",
+                ["Java", "Spring"]
+        )
+
+        when: "O candidato é adicionado à lista"
+        candidatos.add(candidato)
+
+        then: "A lista deve conter exatamente um candidato"
+        candidatos.size() == 1
+
+        and: "O candidato na lista deve ser o mesmo que foi adicionado"
+        candidatos[0] == candidato
+    }
+}
